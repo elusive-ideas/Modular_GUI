8a750779b7b62e1e49d12446c60a6de44eeefb66
David Martinez <david.martinez.anim@gmail.com>
2014-10-22 11:01:08 +0100

    Set title and default size

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import sys
 class MyMainWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
         super(MyMainWindow,self).__init__()
+        self.setWindowTitle('Modular GUI')
+        self.resize(1000,700)
 
 app = QtGui.QApplication(sys.argv)
 form = MyMainWindow()
