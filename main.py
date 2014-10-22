c47d0f2853fabd6b39930ddfe907ba01744f9d29
David Martinez <david.martinez.anim@gmail.com>
2014-10-22 16:03:56 +0100

    Added logging

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from PySide import QtGui
+import logging
 import ui
 import sys
 
-app = QtGui.QApplication(sys.argv)
-form = ui.MyMainWindow()
-form.show()
-app.exec_()
+log = logging.getLogger('modular_GUI')
+
+try:
+    app = QtGui.QApplication(sys.argv)
+    form = ui.MyMainWindow()
+    form.show()
+    app.exec_()
+
+except:
+    log.error('Failed to load the Modular GUI')
+    raise Exception('Failed to load the Modular GUI')
--- ui/__init__.py
+++ ui/__init__.py
@@ -1,8 +1,14 @@
 from PySide import QtGui
+import logging
+
+log = logging.getLogger('modular_GUI.ui')
 
 class MyMainWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
+        log.debug('Initialising the Modular GUI')
         super(MyMainWindow,self).__init__()
         
         self.setWindowTitle('Modular GUI')
         self.resize(1000,700)
+        
+        log.debug('The Modular GUI has been initialised')
