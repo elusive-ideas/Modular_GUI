cbbd9806bfb1f60f98bc77b4fee4db01d9cb7bf1
David Martinez <david.martinez.anim@gmail.com>
2014-10-24 09:33:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Modular_GUI",
-    version="0.0.2",
+    name="modular-gui",
+    version="0.0.1",
     packages=find_packages(),
     install_requires=['PySide', ],
     author="David Martinez",
     author_email="david.martinez.anim@gmail.com",
-    description="Modular GUI using PySide and Yapsy",
+    description="Modular GUI using PySide",
     url="https://github.com/davidmartinezanim/Modular_GUI",
 )
